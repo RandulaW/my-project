35c6126d810e9eb684b8f59979262b396b08b2c2
RandulaW <72642775+RandulaW@users.noreply.github.com>
2020-10-10 21:47:33 +0530

    Update Newmodel.py

--- Newmodel.py
+++ Newmodel.py
@@ -1,23 +1,23 @@
 # Python Implementation of Modiefied Euler method
-def func( x, y ): 
-	return (3*(18-y))
+def func( A, B ): 
+	return (3*(18-B))
  	
 # Function for euler formula 
-def euler( x0, y, h, x ): 
+def euler( A0, B, h, A ): 
 	# Iterating till the point at which we 
 	# need approximation 
-	while x0 < x: 
-		y= y +h*(func(x0+(h/2), y+(h/2)*func(x0,y)))
-		x0 = x0 + h 
+	while A0 < A: 
+		B= B +h*(func(A0+(h/2), B+(h/2)*func(A0,B)))
+		A0 = A0 + h 
 	# Printing approximation 
-	print("Approximate solution at x = ", x, " is ", "%.6f"% y) 
+	print("Approximate solution at A = ", A, " is ", "%.6f"% B) 
 	 
 # Initial Values 
-x0 = 0
-y0 = 14
+A0 = 0
+B0 = 14
 h = 0.2
-x = 1 # Value of x at which we need approximation 
-euler(x0, y0, h, x)
+A = 1 # Value of A at which we need approximation 
+euler(A0, B0, h, A)
 
 
 
